0e1bbb265bae05593e770cdca61ee64d0befa2e9
degoyto <degoyto@gmail.com>
2020-05-06 16:57:23 -0300

    bloco ceara completo

--- __confidencialsite__/componente/PaginaInicial/ceara.php
+++ __confidencialsite__/componente/PaginaInicial/ceara.php
@@ -1,15 +1,228 @@
-<div class="divMissao cearadiv container-flex" id="ceara">
-    <h1>O Ceará</h1>
-    <div class="conteudo container">
-        <div class="imagemLogo imgMapa">
-            <?php include("mapaCeara/mapaCeara.php") ?>
-        </div>
-        <div class="textoPortal textoCeara">
-            <p>
-                O Ceará, um estado do nordeste brasileiro, tem um interior montanhoso e um litoral atlântico com impressionantes falésias vermelhas. O balneário de Canoa Quebrada tem enormes dunas de areia rosadas e a Rua Dragão do Mar, muito movimentada e também conhecida como Broadway. A cidade isolada de Jericoacoara, cercada pelo Parque Nacional de Jericoacoara, repleto de dunas, é famosa por suas ruas de areia, pela prática de windsurfe e pelo pôr do sol incomum em tons de verde.
-            </p>
-            <img src="https://firebasestorage.googleapis.com/v0/b/bird-9fcef.appspot.com/o/mapaCompletoFinal.png?alt=media&token=08a583c9-aa0c-4d1f-8484-8544b89e7045"/>
-        </div>
+<div class="container-flex" id="ceara">
+    <div id="carouselExampleControls" class="carousel slide carouselCeara" data-ride="carousel" data-interval="false">
+        <div class="container-flex carousel-inner">
+            <div class="carousel-item carouselItemCeara cor1 container-flex active">
+                <div class="container conteudoPaginas">
+                    <div class="imgPagina1">
+                        <img src="https://firebasestorage.googleapis.com/v0/b/bird-9fcef.appspot.com/o/maps%2FmapaCompletoFinal.png?alt=media&token=4d3c9b8c-ba26-4a25-b7e5-70f7492f9427"/>
+                    </div>
+                    <div class="container textoPagina1">
+                        <h1> Ceará</h1>
+                        <p>
+                        O Estado do Ceará fica na região Nordeste do Brasil e tem uma área total de 148.886,308 km². Ele
+                        faz divisa com os estados de Pernambuco, Rio Grande do Norte, Paraíba e Piauí. É banhado pelo
+                        oceano Atlântico e tem 573 quilômetros de praias, sendo o quarto maior estado do Nordeste em
+                        tamanho e o 17º entre os estados brasileiros.<br><br>
+
+                        O Estado possui 184 municípios e 20 Microrregiões Administrativas, destacando-se as Regiões
+                        Metropolitanas de Fortaleza, com 19 cidades, e do Cariri, com 9 cidades.
+                        Localizado na esquina do Oceano Atlântico, o Ceará é a porta de entrada no Brasil para Europa,
+                        África e Estados Unidos.<br><br>
+
+                        
+                        </p>
+                    </div>
+                </div>
+
+                
+            </div>
+            <div class="carousel-item cor2 carouselItemCeara container-flex">
+                <div class="container conteudoPaginas">
+                    
+                    <div class="textoPagina2">
+                        <h1> Exportação</h1>
+                        <p>
+                        A rapidez nas exportações marítimas são uma das vantagens e há ainda o menor tempo de trânsito
+                        realizado via modal aéreo, com 6h30min de distância dos três destinos. <br> <br> 
+
+                        O Complexo Industrial e Portuário do Pecém (CIPP) situado a 40 quilômetros de Fortaleza possui
+                        um dos principais portos de exportação do país e abriga, ainda, a única Zona de Processamento de
+                        Exportação (ZPE), primeira a operar no Brasil e com tributação diferenciada.<br> <br> 
+
+                        O Ceará conta também com o Porto do Mucuripe, localizado em Fortaleza, um dos principais portos
+                        de cabotagem do Brasil, detentor de um novo terminal de passageiros capaz de receber grandes
+                        navios de cruzeiro.<br> <br> 
+
+                        
+
+                            
+                            </p>
+                        </div>
+                        <div class="imgPagina2">
+                            <img  src="https://firebasestorage.googleapis.com/v0/b/bird-9fcef.appspot.com/o/maps%2Ftransport.svg?alt=media&token=f79de62f-4bfc-4ff0-8489-06bd2113d621" alt="Segundo Slide">
+                        
+                        </div>
+                    </div>
+                    
+                </div>
+                
         
+                <div class="carousel-item carouselItemCeara cor3 container-flex">
+                    <div class="container conteudoPaginas">
+                        <div class="imgPagina1">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/bird-9fcef.appspot.com/o/maps%2Ffashion.svg?alt=media&token=bc8f2b14-994d-41c0-802a-a91dc92cceb4"/>
+                        </div>
+                        <div class="container textoPagina1">
+                            <h1> Ensino</h1>
+                            <p>
+                            No Ceará funcionam as 24 melhores escolas públicas do ensino fundamental, além de 77 entre as
+                            100 públicas mais bem ranqueadas do Brasil, segundo levantamento do Índice de Desenvolvimento
+                            da Educação Básica (Ideb).<br><br> Atualmente, o Estado possui 119 Escolas Estaduais de Educação
+                            Profissional (EEEP), contabilizando 50 mil alunos matriculados em um total de 91 municípios.
+                            População estimada pelo IBGE (2019): 9.132.078 pessoas
+                            
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="carousel-item carouselItemCeara cor1 container-flex">
+                    <div class="container conteudoPaginas">
+                        <div class="imgPagina1 imgPagina4">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/bird-9fcef.appspot.com/o/maps%2FnorteMapa.svg?alt=media&token=42045636-5a4f-4eba-81e3-859749594638"/>
+                            
+                        </div>
+                        <div class="container textoPagina1 textoPagina4">
+                            <h1> MESORREGIÕES NORTE</h1>
+                            <p>
+                                    Microrregiões:<br>
+                                > Itapipoca: Amontada, Itapipoca, Trairi<br>
+                                    > Baixo Curu: Paracuru, Paraipaba, São Gonçalo do Amarante<br>
+                                    > Uruburetama: Itapajé, Tururu, Umirim, Uruburetama<br>
+                                    Médio Curu: Apuiarés, General Samapio, Pentencoste, São Luís do Curu, Tejuçuoca<br>
+                                    > Canindé: Canindé, Caridade, Itatira, Paramoti<br>
+                                    > Baturité: Acarape, Aracoiaba, Aratuba, Baturit, Capsitrano, Guaramiranga, Itapiúna,
+                                    Mulungu, Pacoti, Palmácia, Redenção.<br>
+                                    > Chorozinho: Barreira, Chorozinho, Ocara<br>
+                                    > Cascavel: Beberib, Cascavel, Pindoretama<br>
+                                    <b>REGIÃO METROPOLITANA DE FORTALEZA:</b><br>
+                                    > Fortaleza: Aquiraz, Caucaia, Eusébio, Fortaleza, Guaiúba, Itaitinga, Maracanaú,
+                                    Maranguape, Pacatuba.<br>
+                                    > Pacajus: Horizonte, Pacajus<br><br>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="carousel-item carouselItemCeara cor2 container-flex">
+                    <div class="container conteudoPaginas">
+                        <div class="imgPagina1 imgPagina4">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/bird-9fcef.appspot.com/o/maps%2FnoroesteMapa.svg?alt=media&token=52e50f03-996b-4cdb-aba4-dd22c38eb0d2"/>
+                        </div>
+                        <div class="container textoPagina1 textoPagina4">
+                            <h1> MESORREGIÕES NOROESTE</h1>
+                            <p>
+                                
+                                    Microrregiões:<br>
+                                    > <b>Litoral de Camocim e Acaraú:</b> Acaraú, Barroquinha, Bela Cruz, Camocim, Chaval, Cruz
+                                    Granja, Itarema, Jijoca de Jericoacoara, Marco, Martinópole, Morrinhos.<br>
+                                    > <b>Ibiapaba:</b> Carnaubal, Croatá, Guaraciaba do Norte, Ibiapina, São Benedito, Tianguá,
+                                    Ubajara, Viçosa do Ceará<br>
+                                    > Coreaú:</b> Coreaú, Frecheirinha, Moraújo, Uruoca<br>
+                                > <b>Meruoca:</b> Alcântaras, Meruoca<br>
+                                    > <b>Sobral:</b> Cariré, Forquilha, Graça, Groaíras, Irauçuba, Massapê, Miraíma, Mucambo,
+                                    Pacujá, Santana do Acaraú, Senadpr Sá, Sobral<br>
+                                > <b>Ipu:</b> Ipu, Ipueiras, Pires Ferreira, Poranga, Reriutaba, Varjota<br>
+                                    > <b>Santa Quitéria:</b> Catunda, Hidrolândia, Santa Quitéria<br>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item carouselItemCeara cor3 container-flex">
+                    <div class="container conteudoPaginas">
+                        <div class="imgPagina1 imgPagina4">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/bird-9fcef.appspot.com/o/maps%2FsertoesMapa.svg?alt=media&token=6b9f90ed-18dd-4f09-bbc4-6631cce37ca3"/>
+                        </div>
+                        <div class="container textoPagina1">
+                            <h1> MESORREGIÕES SERTÕES</h1>
+                            <p>
+                                
+                                ><b>Sertão de Crateús:</b> Ararendá, Crateús, Independência, Ipaporanga, Monsenhor Tabosa,
+                                Nova Russas, Novo Oriente, Quiterianópolis, Tamboril.<br>
+                                ><b>Sertão de Quixeramobim:</b> Banabuiú, Boa Viagem, Chor, Ibaretama, Quixadá,
+                                Quixeramobim.><br>
+                                ><b>Sertão de Inhamuns:</b> Aiuaba, Arneiroz, Catarina, Paramb, Saboeiro, Tauá.<br>
+                                ><b>Sertão de Senador Pompeu:</b> Acopiara, Deputado Irapuan Pinheiro, Milhã, Mombaça,
+                                Pedra Branca, Piquet Carneiro, Senador Pompeu, Solonópole.<br>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item carouselItemCeara cor1 container-flex">
+                    <div class="container conteudoPaginas">
+                        <div class="imgPagina1 imgPagina4">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/bird-9fcef.appspot.com/o/maps%2FjaguaribeMapa.svg?alt=media&token=3471ec07-f881-4cd3-8407-42f9d3fb824f"/>
+                        </div>
+                        <div class="container textoPagina1">
+                            <h1>MESORREGIÕES REGIÃO JAGUARIBE </h1>
+                            <p>
+                                
+                                > <b>Litoral de Aracati:</b> Aracati, Fortim, Icapuí, Itaiçaba.<br>
+                                > <b>Baixo Jaguaribe:</b> Alto Santo, Ibicuitinga, Jaguaruana, Limoeiro do Norte, Morada Nova,
+                                Palhano, Quixeré, Russas, São João do Jaguaribe, Tabuleiro do Norte.<br>
+
+                                > <b>Médio Jaguaribe:</b> Jaguaretama, Jaguaribara, Jaguaribe.<br>
+                                > <b>Serra do Pereiro:</b> Ereré, Iracema, Pereiro, Potiretama.<br>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item carouselItemCeara cor2 container-flex">
+                    <div class="container conteudoPaginas">
+                        <div class="imgPagina1 imgPagina4">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/bird-9fcef.appspot.com/o/maps%2FcentroMapa.svg?alt=media&token=74ec828a-3c8f-4605-bf9c-f76178039fc8"/>
+                        </div>
+                        <div class="container textoPagina1">
+                            <h1>MESORREGIÕES CENTRO-SUL</h1>
+                            <p>
+                                
+                                ><b>Iguatu:</b> Cedro, Icó, Iguatu, Orós, Quixelô.<br>
+                                ><b>Várzea Alegre:</b> Antonina do Norte, Cariús, Jucás, Tarrafas, Várzea Alegre.<br>
+                                ><b>Lavras da Mangabeira:</b> Baixio, Ipaumirim, Lavras da Mangabeira, Umari.<br>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item carouselItemCeara cor3 container-flex">
+                    <div class="container conteudoPaginas">
+                        <div class="imgPagina1 imgPagina4">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/bird-9fcef.appspot.com/o/maps%2FsulMapa.svg?alt=media&token=f90068d4-a187-4c92-b253-ac83a3facc43"/>
+                        </div>
+                        <div class="container textoPagina1">
+                            <h1>MESORREGIÕES SUL</h1>
+                            <p>
+                               
+                                ><b>Chapada do Araripe:</b> Araripe, Assaré, Campos Sales, Potengi, Salitre.<br>
+                                ><b>Caririaçu:</b> Altaneira, Caririaçu, Farias Brito, Granjeiro.<br>
+                                ><b>Barro:</b> Aurora, Barr, Mauriti.<br>
+                                ><b>Cariri:</b> Barbalha, Crato, Jardim, Juazeiro do Norte, Missão Velha, Nova Olinda,
+                                Porteiras – Santana do Cariri<br>
+                                ><b>Brejo Santo:</b> Abaiara, Brejo Santo, Jati, Milagres, Penaforte.<br>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            
+            
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Anterior</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Próximo</span>
+        </a>
+
     </div>
 </div>
+<script>
+    jQuery(window).load(function() {
+ 
+    /*
+        Stop carousel
+    */
+    $('.carousel').carousel('pause');
+
+    });
+</script>
