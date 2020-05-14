d71abbf335b04b972daf758ef93ddd52f881fe19
degoyto <degoyto@gmail.com>
2020-05-14 19:38:17 -0300

    pagina inicial feita

--- __confidencialsite__/componente/PaginaInicial/cidadesDestaque/cidades.php
+++ __confidencialsite__/componente/PaginaInicial/cidadesDestaque/cidades.php
@@ -1,10 +1,38 @@
+<?php
+function limita_caracteres($texto, $limite, $quebra = true) {
+    $tamanho = strlen($texto);
+
+    // Verifica se o tamanho do texto é menor ou igual ao limite
+    if ($tamanho <= $limite) {
+        $novo_texto = $texto;
+    // Se o tamanho do texto for maior que o limite
+    } else {
+        // Verifica a opção de quebrar o texto
+        if ($quebra == true) {
+            $novo_texto = trim(substr($texto, 0, $limite)).'... <b>Clique no município para Ler Mais</b>';
+        // Se não, corta $texto na última palavra antes do limite
+        } else {
+            // Localiza o útlimo espaço antes de $limite
+            $ultimo_espaco = strrpos(substr($texto, 0, $limite), ' ');
+            // Corta o $texto até a posição localizada
+            $novo_texto = trim(substr($texto, 0, $ultimo_espaco)).'... Clique no município para Ler Mais';
+        }
+    }
+
+    // Retorna o valor formatado
+    return $novo_texto;
+}
+?>
+
+
+
+
 <div class="destaqueTexto">
     <div id="cidade0" class="cidadeAtivada">
         
         <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
             <br><br>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
             
         </p>
     </div>
@@ -15,7 +43,7 @@
             "<div id='cidade$idAtual' class='cidadeDesativada'>
                 <h1>".$exibeTodasCidades['nome']."</h1>
                 <p>".
-                    $exibeTodasCidades['resumo']."
+                    limita_caracteres($exibeTodasCidades['resumo'],500)."
 
                 </p>
             </div>";
@@ -24,11 +52,6 @@
     
     ?>
 
-    <div id="cidade2" class="cidadeDesativada">
-        <h1>Maracanaú</h1>
-        <p>
-            Maracanaú é um município brasileiro do estado do Ceará, Região Nordeste do país. Fica localizado na Região Metropolitana de Fortaleza, a 24 km da capital. É o maior centro industrial do estado e conhecida como a maior cidade-dormitório do Ceará. É também a terra adotiva do escritor Rodolfo Teófilo.
-        </p>
-    </div>
+    
     
 </div>
--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ var atual = 1;
 
     function abreGif(){
         document.getElementById("gifLogo").src = ""
-        document.getElementById("gifLogo").src = "https://firebasestorage.googleapis.com/v0/b/bird-9fcef.appspot.com/o/robotoMaiusculo.gif?alt=media&token=3c083925-8f77-4126-b5ca-d2b9421906b0"
+        document.getElementById("gifLogo").src = "https://firebasestorage.googleapis.com/v0/b/o-portal-do-ceara.appspot.com/o/outros%2Flogo.gif?alt=media&token=efe6027e-9f19-4da4-b6c8-6b4353613d0e"
         
         
         document.getElementById("textoCapa").classList.remove("fechaTudo");
@@ -21,7 +21,7 @@ var atual = 1;
         
         setTimeout( function() {
             gifAtivo = 0;
-            console.log("tempo acabou");
+            
             document.getElementById("item"+atual).classList.remove("active");
             atual = 1;
             document.getElementById("item"+atual).classList.add("active");
@@ -45,7 +45,7 @@ var atual = 1;
                 atual = 1;
                 gifAtivo = 1;
                 abreGif();
-                console.log("gif atual>6 "+gifAtivo)
+               
             }
             // console.log("depois do if "+atual+" "+gifAtivo);
             document.getElementById("item"+atual).classList.add("active");
@@ -85,7 +85,7 @@ function selecionado(recebido){
         }
     }
     
-    console.log("clique: "+duploClique)
+    
     anterior = recebido.id;
     
 }
@@ -109,74 +109,21 @@ function voltarCor(recebido){
 
 function abreDivInfo(idRecebido){
     
-    console.log("abre cidade"+idRecebido)
+    
     document.getElementById("cidade" + idRecebido).classList.remove("cidadeDesativada");
     document.getElementById("cidade" + idRecebido).classList.add("cidadeAtivada");
     
 }
 function fechaDivInfo(idRecebido){
     
-    console.log("fecha cidade"+idRecebido)
+    
     document.getElementById("cidade"+idRecebido).classList.remove("cidadeAtivada");
     document.getElementById("cidade"+idRecebido).classList.add("cidadeDesativada");
    
     
 }
-function redireciona(){
-    console.log("redireciona")
-}
 
 
 
 
 
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyAw6xl5wzRaJDJPWVqrrWlhKBiR3EvZO0M",
-    authDomain: "jornal-porto-do-pecem.firebaseapp.com",
-    databaseURL: "https://jornal-porto-do-pecem.firebaseio.com",
-    projectId: "jornal-porto-do-pecem",
-    storageBucket: "jornal-porto-do-pecem.appspot.com",
-    messagingSenderId: "117754526845",
-    appId: "1:117754526845:web:4f764a08dd39dbbc9f0514",
-    measurementId: "G-KWJJNLNFEC"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-function uploadImage(){
-    
-    document.getElementById('caixa-confirma').classList.remove("desativado");
-    document.getElementById('caixa-confirma').classList.add("ativo");
-    const ref = firebase.storage().ref()
-    const file = document.querySelector('#foto').files[0]
-    const name = new Date() + '-' + file.name
-    const metadata = {
-        contentType:file.type
-    }
-
-    const task = ref.child("noticias/"+name).put(file, metadata)
-    task.then(snapshot => snapshot.ref.getDownloadURL()).then(url =>{
-        const imageelement = document.querySelector("#imagem")
-        const testeelement = document.querySelector("#teste")
-
-
-        document.getElementById('img-processando').classList.remove("ativo");
-        document.getElementById('img-processando').classList.add("desativado");
-
-        document.getElementById('img-concluido').classList.remove("desativado");
-        document.getElementById('img-concluido').classList.add("ativo");
-
-        document.getElementById('btn-confirma').classList.remove("desativado");
-        document.getElementById('btn-confirma').classList.add("ativado");
-        
-        imageelement.src = url
-        testeelement.value = url
-        
-        
-    })
-}
-
-
-
-firebase.analytics();
